cb1240fe698568e76eb2888c80c17f99f6d07ef8
Ilia Ozhmegov <ElijahOzhmegov@gmail.com>
2019-03-06 14:44:09 +0300

    drawing of x axe of time is okay

--- grapher.py
+++ grapher.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import csv_parser as csv_pr
-from datetime import datetime
 import numpy as np
 import matplotlib.dates as mdates
+from datetime import datetime
 
 def conv_24h(timestamp):
     dt = datetime.fromtimestamp(timestamp / (10 ** 6))
@@ -12,7 +12,12 @@ def draw(Xparam, Yparam):
     Xdata = [(conv_24h(int(val))) for val in Xparam.data]
     Ydata = [float(val) for val in Yparam.data]
 
-    plt.plot(Xdata, Ydata)
+    fig, ax = plt.subplots()
+    ax.plot(Xdata, Ydata)
+
+    h_fmt = mdates.DateFormatter('%H:%M:%S')
+    ax.xaxis.set_major_formatter(h_fmt)
+    _ = plt.xticks(rotation=30)
 
     plt.xlabel(Xparam.name)
     plt.ylabel(Yparam.name)
