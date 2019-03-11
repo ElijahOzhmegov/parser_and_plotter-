1636b6c6e499653ae5839b20e0fc523c0b24447a
Ilia Ozhmegov <ElijahOzhmegov@gmail.com>
2019-03-11 09:17:54 +0300

    before the try to change architecture

--- config_parser.py
+++ config_parser.py
@@ -1,25 +1,48 @@
-str_path = "path"
+import os
 
-def search_for_path(file):
-    for line in file.readlines():
+class Graph:
+    def __init__(self):
+        self.name = ''
+
+        self.Xname = ''
+        self.Ynames = []
+
+
+def searching_for(key, file):
+    while True:
+        line = file.readline()
         words = line.split("=")
 
         for word in words:
             uncluttered_word = word.replace(" ", "")
 
-            if uncluttered_word == str_path:
+            if uncluttered_word == key:
                 path = words[1].split("\"")
                 return path[1]
 
+    return 0
+
+
+def scanning_for_graph_data(file):
+    print(file.readlines())
 
 def main():
     file = open("csv_config.config")
 
-    path = search_for_path(file)
+    path = searching_for("path", file)
 
-    with open(path) as csv_file:
-        print(csv_file.readline())
-    print(path)
+    if path == 0:
+        print("Unfortunately variable \"path\" was not defined!")
+        print("Make sure that variable \"path\" was written correct.")
+    elif os.path.isfile(path):
+
+        scanning_for_graph_data(file)
+
+        with open(path) as csv_file:
+            print(csv_file.readline())
+        print(path)
+    else:
+        print("Unfortunately defined path: \"" + path + "\" does not exist!")
 
     file.close()
 
